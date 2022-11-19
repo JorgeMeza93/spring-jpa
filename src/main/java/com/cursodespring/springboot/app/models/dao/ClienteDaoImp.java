958fb53c0f6f108efa9a3eb624c9c397ff61d40a
Jorge Meza Bello <jorberiver@gmail.com>
2022-11-18 21:42:03 -0600

    Implementacion de interfaz Dao y entity manager

---
+++ src/main/java/com/cursodespring/springboot/app/models/dao/ClienteDaoImp.java
@@ -0,0 +1,23 @@
+package com.cursodespring.springboot.app.models.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.cursodespring.springboot.app.models.entity.Cliente;
+
+@Repository
+public class ClienteDaoImp implements IClienteDao{
+	@PersistenceContext
+	private EntityManager em;
+	@Transactional(readOnly = true)
+	@Override
+	public List<Cliente> findAll() {
+		return em.createQuery("From Cliente").getResultList();
+	}
+
+}
---
+++ src/main/java/com/cursodespring/springboot/app/models/dao/IClienteDao.java
@@ -0,0 +1,9 @@
+package com.cursodespring.springboot.app.models.dao;
+
+import java.util.List;
+
+import com.cursodespring.springboot.app.models.entity.Cliente;
+
+public interface IClienteDao {
+	public List<Cliente> findAll();
+}
