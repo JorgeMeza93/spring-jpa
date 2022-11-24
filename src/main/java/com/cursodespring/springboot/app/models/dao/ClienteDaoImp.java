c88c8f3ca49fd083e26adb8976b81e98e64c5090
Jorge Meza Bello <jorberiver@gmail.com>
2022-11-23 23:28:40 -0600

    guardar cliente listo

--- src/main/java/com/cursodespring/springboot/app/models/dao/ClienteDaoImp.java
+++ src/main/java/com/cursodespring/springboot/app/models/dao/ClienteDaoImp.java
@@ -19,5 +19,10 @@ public class ClienteDaoImp implements IClienteDao{
 	public List<Cliente> findAll() {
 		return em.createQuery("From Cliente").getResultList();
 	}
+	@Override
+	@Transactional
+	public void save(Cliente cliente) {
+		em.persist(cliente);
+	}
 
 }
--- src/main/java/com/cursodespring/springboot/app/models/dao/IClienteDao.java
+++ src/main/java/com/cursodespring/springboot/app/models/dao/IClienteDao.java
@@ -6,5 +6,5 @@ import com.cursodespring.springboot.app.models.entity.Cliente;
 
 public interface IClienteDao {
 	public List<Cliente> findAll();
-	
+	public void save(Cliente cliente);
 }
